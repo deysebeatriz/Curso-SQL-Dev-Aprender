95d6f9a1288752cf47887891e6ea03079b13bbd2
Deyse Beatriz <dbfdj.eng20@uea.edu.br>
2026-05-20 14:49:32 -0400

    Aula 02: Resolução do Desafio de Marketing
    Adiciona o script de resolução da demanda prática solicitada pela equipe de marketing:
    
    Uso da cláusula SELECT para trazer múltiplas colunas simultaneamente.
    
    Extração das colunas FirstName e LastName da tabela Person.Person para listagem de clientes.

---
+++ Desafios/aula_02_desafio_marketing.sql
@@ -0,0 +1,5 @@
+-- DESAFIO 01: Marketing
+-- O Pedido: A equipe de marketing precisa fazer uma pesquisa sobre os nomes mais comuns dos clientes e precisa do nome e sobrenome de todos os clientes cadastrados.
+
+SELECT FirstName, LastName
+FROM Person.Person;
