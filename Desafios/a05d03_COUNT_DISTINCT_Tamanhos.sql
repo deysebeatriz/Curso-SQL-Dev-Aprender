ce1042c353a476e600d125ca35034f7ca0d44c01
Deyse Beatriz <dbfdj.eng20@uea.edu.br>
2026-05-20 16:07:19 -0400

    Update a05d03_COUNT_DISTINCT_Tamanhos.sql

--- Desafios/a05d03_COUNT_DISTINCT_Tamanhos.sql
+++ Desafios/a05d03_COUNT_DISTINCT_Tamanhos.sql
@@ -1,4 +1,4 @@
--- Aula 05. Desafio 02: Quantos tamanhos diferentes de produtos temos cadastrados em nossa tabela de produtos?
+-- Aula 05. Desafio 03: Quantos tamanhos diferentes de produtos temos cadastrados em nossa tabela de produtos?
 
 SELECT count(DISTINCT Size)
 FROM Production.Product
