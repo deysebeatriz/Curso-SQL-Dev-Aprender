5666e12ca9635b5d74a64fd989529d8c5a62255a
Deyse Beatriz <dbfdj.eng20@uea.edu.br>
2026-05-20 16:04:12 -0400

    Aula 05: Resolução do Desafio 3 (COUNT DISTINCT)
    Resolução do terceiro desafio da Aula 05:
    - Uso do COUNT(DISTINCT Size) na tabela Production.Product para contar apenas as variações únicas de tamanho.
    - Resultado atualizado e documentado (18 tamanhos diferentes).

---
+++ Desafios/a05d03_COUNT_DISTINCT_Tamanhos.sql
@@ -0,0 +1,6 @@
+-- Aula 05. Desafio 02: Quantos tamanhos diferentes de produtos temos cadastrados em nossa tabela de produtos?
+
+SELECT count(DISTINCT Size)
+FROM Production.Product
+
+-- R: 211 tamanhos
