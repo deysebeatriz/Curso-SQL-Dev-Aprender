b2ee93ccea1144ac3d4bddac9a46a787c5a0bec8
Deyse Beatriz <dbfdj.eng20@uea.edu.br>
2026-05-26 21:59:28 -0400

    Aula 13: Resolução do Desafio 3 (Agregação com ordenação DESC)
    - Utilização de SUM(LineTotal) agrupado por ProductID para identificar o faturamento total por item.
    - Aplicação de ORDER BY ... DESC junto ao TOP 10 para exibir corretamente os 10 produtos com maior valor acumulado.

---
+++ Desafios/a13d03_GROUPBY_SUM_Top10.sql
@@ -0,0 +1,6 @@
+-- AULA 13 - DESAFIO 03 - Quais foram as 10 vendas que, no total, tiveram os maiores valores de venda (line total) por produto, do maior valor para o menor
+
+SELECT TOP 10 ProductID, SUM(LineTotal) AS SomaLineTotal
+FROM Sales.SalesOrderDetail
+GROUP BY ProductID
+ORDER BY SomaLineTotal DESC
